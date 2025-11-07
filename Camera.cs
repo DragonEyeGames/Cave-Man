e56cf9cc5b17d17c353cd6b9f83021266efdf902
Taxalottle Syndicate <bthole41@gmail.com>
2025-11-06 20:30:34 -0500

    Camera zooming?

--- Camera.cs
+++ Camera.cs
@@ -17,7 +17,6 @@ public partial class Camera : Camera2D
 			x += newChild.GlobalPosition.X;
 		}
 		x/=GetNode("../Markers").GetChildren().Count;
-		GD.Print(x);
 		
 		float y = 0.0f;
 		foreach(Marker2D child in GetNode("../Markers").GetChildren()) {
@@ -25,7 +24,20 @@ public partial class Camera : Camera2D
 			y += newChild.GlobalPosition.Y;
 		}
 		y/=GetNode("../Markers").GetChildren().Count;
-		GD.Print(y);
 		GlobalPosition = new Vector2(x, y);
+		
+		var biggestX = 0.0f;
+		foreach(Marker2D child in GetNode("../Markers").GetChildren()) {
+			Node2D newChild = child as Node2D;
+			if(MathF.Abs(newChild.GlobalPosition.X)>MathF.Abs(biggestX)) {
+				biggestX=newChild.GlobalPosition.X;
+			}
+		}
+		GD.Print("AHEAHE" + (biggestX-GlobalPosition.X));
+		if(MathF.Abs(biggestX-GlobalPosition.X) > GetViewport().GetVisibleRect().Size.X/2) {
+			Zoom = new Vector2(Zoom.X - .1f, Zoom.Y - .1f);
+		}
+		GD.Print(GetViewport().GetVisibleRect().Size.X + " AAH " + MathF.Abs(biggestX-GlobalPosition.X));
+		x/=GetNode("../Markers").GetChildren().Count;
 	}
 }
