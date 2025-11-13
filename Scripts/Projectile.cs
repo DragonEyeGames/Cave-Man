f994e0374e667ba7250bfb3545ccfbd521417c81
Taxalottle Syndicate <133176852+DragonEyeGames@users.noreply.github.com>
2025-11-13 11:51:56 -0500

    Extra organization

--- Scripts/Projectile.cs
+++ Scripts/Projectile.cs
@@ -1,21 +1,22 @@
 using Godot;
 using System;
 
-public partial class Rock : Projectile
+public abstract partial class Projectile : RigidBody2D
 {
 	public int playerID = 0;
 	private double spawnedInTime = 0.0;
 	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
-	{
-
-	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _Process(double delta)
 	{
 		spawnedInTime+=delta;
 	}
+
+	public void count(double delta)
+	{
+        spawnedInTime += delta;
+    }
 
 	public void BodyEntered(Node2D body)
 	{
