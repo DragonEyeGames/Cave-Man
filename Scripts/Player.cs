d90bc3aef5d58fd809517ecbb7c0b51c1eef8526
Taxalottle Syndicate <133176852+DragonEyeGames@users.noreply.github.com>
2025-12-04 11:26:14 -0500

    Fixed player workings

--- Scripts/Player.cs
+++ Scripts/Player.cs
@@ -132,8 +132,8 @@ public partial class Player : CharacterBody2D
 			}
 			GetNode<Sprite2D>("Icon/Arm").Visible=false;
 			GetNode<Sprite2D>("Icon/DummyArm").Visible=true;
-			GetNode<Sprite2D>("Icon/DummyArm").LookAt(GetNode<ColorRect>("ColorRect").Position);
-			GetNode<Sprite2D>("Icon/DummyArm").RotationDegrees+=90;
+			GetNode<Sprite2D>("Icon/DummyArm").LookAt(GetNode<ColorRect>("ColorRect").GlobalPosition);
+			GetNode<Sprite2D>("Icon/DummyArm").RotationDegrees-=90;
 			GD.Print(GetNode<Sprite2D>("Icon/DummyArm").RotationDegrees);
 			GetNode<Node2D>("Arrow").LookAt(GetNode<ColorRect>("ColorRect").GlobalPosition);
 			rockVelocity += (float)delta * 6;
